01e2cdea7760d21bbcae5f0a681791cfca959c15
Richard Kim <rk.sungjin@gmail.com>
2018-09-22 16:15:03 -0700

    Day1 LetandConst

---
+++ Day-1/LetandConst.js
@@ -0,0 +1,8 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+        const PI = Math.PI;
+        var r = parseInt(readLine());
+        // Print the area of the circle:
+        var area = PI * r * r;
+        console.log(area);
+}
